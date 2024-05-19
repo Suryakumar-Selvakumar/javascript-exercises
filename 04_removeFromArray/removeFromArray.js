9255e63e2d3dae233c5c2dcd29c2beb3489bbc96
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-05-19 17:54:16 -0600

    Finish Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removableElements) {
+  const newArray = [];
+  array.forEach((element) => {
+    if (!removableElements.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
