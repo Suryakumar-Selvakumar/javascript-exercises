bc3ea77e00af29386f21ee68c960241233545906
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-05-20 11:32:23 -0600

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const punctuations = [
+    ".",
+    "!",
+    "?",
+    ",",
+    ";",
+    ":",
+    "—",
+    "-",
+    "(",
+    ")",
+    "[",
+    "]",
+    "{",
+    "}",
+    "...",
+    " ",
+  ];
+  let cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((element) => !punctuations.includes(element))
+    .join("");
+  reversedString = cleanedString.split("").reverse().join("");
+  console.log(cleanedString);
+  console.log(reversedString);
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
