6a5a93da37de3921ee64e66cde4d9a41023ad385
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-05-20 15:18:08 -0600

    Finish Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  n = +n;
+  const array = [0, 1];
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  if (n < 0) return "OOPS";
+  for (x = 2; x <= n; x++) {
+    array[x] = array[x - 1] + array[x - 2];
+  }
+  return array[n];
 };
 
 // Do not edit below this line
