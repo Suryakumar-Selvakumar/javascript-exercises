2e406c59278ce1f895236bb9e40558ea176f1c77
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-05-20 08:11:26 -0600

    Finish Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let normalSum = 0;
+  let reverseSum = 0;
+  let oldStart = start;
+  let oldEnd = end;
+  if (!(typeof start === "number") || !(typeof end === "number")) {
+    return "ERROR";
+  } else {
+    for (start; start <= end; start++) {
+      normalSum += start;
+    }
+    for (end; end <= oldStart; end++) {
+      reverseSum += end;
+    }
+    if (normalSum < 0 || reverseSum < 0) {
+      return "ERROR";
+    } else {
+      if (oldStart < oldEnd) {
+        return normalSum;
+      } else {
+        return reverseSum;
+      }
+    }
+  }
 };
 
 // Do not edit below this line
